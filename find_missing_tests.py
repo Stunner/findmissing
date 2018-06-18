b01b709093a4c8fa03f5385f3903a3b556935965
Aaron <ajjubbal@gmail.com>
2018-06-17 21:42:20 -0700

    Added unit test for print_diff() method.

--- find_missing_tests.py
+++ find_missing_tests.py
@@ -1,4 +1,6 @@
 import unittest
+import io
+# import sys
 from find_missing import *
 
 
@@ -30,6 +32,22 @@ class FindMissingTests(unittest.TestCase):
         line = "1krMK432WG2CaFbiNnyBewaksT09he5   DSC003.aa   bin    1.1 GB   2018-06-12 20:54:22"
         pattern = re.compile("(DSC)003\.")
         self.assertRaises(ValueError, get_specified_num, pattern, line)
+
+    def test_print_diff(self):
+        # reference: https://stackoverflow.com/a/34738440/347339
+        captured_output = io.StringIO()  # Create StringIO object
+        sys.stdout = captured_output  # and redirect stdout.
+        print_diff(12, 1, 8, 0)  # Call function.
+        output = "9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n"
+        self.assertEqual(captured_output.getvalue(), output)
+        sys.stdout = sys.__stdout__
+
+        captured_output = io.StringIO()  # Create new StringIO object (required to reset previous value)
+        sys.stdout = captured_output  # and redirect stdout.
+        print_diff(5, -1, 8, 1)  # Call function.
+        output = "7\n6\n5\n4\n"
+        self.assertEqual(captured_output.getvalue(), output)
+        sys.stdout = sys.__stdout__
 
 
 if __name__ == '__main__':
